7232ae1429189fc5a058e016f2fd86753b031d2c
PavlovaVeronikka <toss.chubs.08@icloud.com>
2025-12-17 09:53:32 +0100

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NebulaSurvey from "./app/nebulaSurvey";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <NebulaSurvey />
+  </React.StrictMode>
+);
